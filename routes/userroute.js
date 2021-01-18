65f723f6f482be4da7606e161c8d62288a5ce03f
tinatrinh1012 <nhungoctrinhle@gmail.com>
2021-01-17 21:37:21 -0600

    install packages

---
+++ routes/userroute.js
@@ -0,0 +1,5 @@
+const express = require('express')
+var app = express()
+const mongoose = require('mongoose')
+const router = express.Router()
+
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-// hello?
+const express = require('express');
+var app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const router = express.Router()
+const userroute = require('./routes/userroute')
+app.use('/api/user', userroute)
+
+
+
+app.get('/', function(req, res) {
+    res.send('This is home page')
+})
+
+
+app.listen(5000, ()=>{
+    console.log('Server started on port 5000');
+})
